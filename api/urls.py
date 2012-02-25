2190bc548c54eb248433da9adee442359f1ab18f
Douglas Kastle <douglas.kastle@gmail.com>
2012-02-26 01:27:02 +1100

    Moved locations of code around

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import patterns, url
 from piston.resource import Resource
 from piston.authentication import HttpBasicAuthentication
 from api.handlers import IngestHandler
 
 urlpatterns = patterns('',
-	url(r'^ingest/', Resource(IngestHandler), { 'emitter_format': 'json' }),
+	url(r'^upload/', Resource(IngestHandler), { 'emitter_format': 'json' }),
 )
--- gigs/urls.py
+++ gigs/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from piston.resource import Resource
 from piston.authentication import HttpBasicAuthentication
-from gigs.handlers import IngestHandler
 from views import *
 
 urlpatterns = patterns('',
@@ -9,5 +8,4 @@ urlpatterns = patterns('',
     url(r'^upcoming/$', ical, name='ical-upcoming'),
     url(r'^performance/(\d+)/$', performance, name='performance'),
 #     url(r'^performance/$', performances, name='performances'),
-	url(r'^ingest/', Resource(IngestHandler), { 'emitter_format': 'json' }),
 )
